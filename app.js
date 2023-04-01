df6292b685a60f4edb3f18ecaf88bcad02354b7f
Adhil-bit <adhilashraf76@gmail.com>
2023-04-01 17:09:40 +0530

    adding mysql

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const app =express();
 //body parser
 app.use(express.json());
 
+//mysql
+const myDB = mysql.createPool({
+    host:'127.0.0.1',
+    port:3306,
+    user:'adhil',
+    password:'password123',
+    database:'todoapp',
+})
+
+
 // server
 let PORT = process.env.PORT|| 3003;
 app.listen(PORT,()=> console.log(`server hosting on port ${PORT}`));
